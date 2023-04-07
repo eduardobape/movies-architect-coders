b8655bb236bb9fae3b000d368e9436cc015e9029
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-07 17:39:57 +0200

    Create custom okHttp client and add it to retrofit as an interceptor.
    This custom okHttp client is created to add the TheMovieDB api key
    as a query parameter into all URLs used to make requests to the
    TheMovideDB web service using Retrofit.

--- app/src/main/java/com/example/mymovies/data/apiservices/RetrofitBuilder.kt
+++ app/src/main/java/com/example/mymovies/data/apiservices/RetrofitBuilder.kt
@@ -1,12 +1,29 @@
 package com.example.mymovies.data.apiservices
 
+import com.example.mymovies.BuildConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitBuilder {
 
+	val networkClient: OkHttpClient = OkHttpClient.Builder()
+		.addInterceptor { chain ->
+			val originalRequest = chain.request()
+			val originalHttpUrl = originalRequest.url()
+
+			val modifiedUrl = originalHttpUrl.newBuilder()
+				.addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+				.build()
+
+			val modifiedRequest = originalRequest.newBuilder().url(modifiedUrl).build()
+			chain.proceed(modifiedRequest)
+		}
+		.build()
+
 	val retrofit: Retrofit = Retrofit.Builder()
 		.baseUrl(theMovieDbBaseUrl)
+		.client(networkClient)
 		.addConverterFactory(MoshiConverterFactory.create())
 		.build()
 }
