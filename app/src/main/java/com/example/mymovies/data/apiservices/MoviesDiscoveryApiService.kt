cf967df45d95322b8fccf45107100b164b2156dd
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-09 21:40:45 +0200

    Add method discoverMoviesByYear to get movies filtered by year from /discover/movie API resource

--- app/src/main/java/com/example/mymovies/data/apiservices/MoviesDiscoveryApiService.kt
+++ app/src/main/java/com/example/mymovies/data/apiservices/MoviesDiscoveryApiService.kt
@@ -1,4 +1,16 @@
 package com.example.mymovies.data.apiservices
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MoviesDiscoveryApiService {
+
+	@GET("/discover/movie")
+	suspend fun discoverMoviesByYear(
+		@Query("year") year: Int,
+		@Query("page") page: Int,
+		@Query("sort_by") sortBy: String = "release_date.desc",
+		@Query("region") region: String = "ES",
+		@Query("language") language: String = "es-ES"
+	): List<MoviesDiscoveryResult>
 }
