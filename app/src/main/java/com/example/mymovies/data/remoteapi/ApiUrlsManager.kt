c1eeb0549d7a25101d4556f63b6f0c14f9d80c62
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-11 13:15:40 +0200

    Convert imageBaseUrl variable to a constant.
    The value of this constant is the base URL of an image in the TheMovieDB API.

--- app/src/main/java/com/example/mymovies/data/remoteapi/ApiUrlsManager.kt
+++ app/src/main/java/com/example/mymovies/data/remoteapi/ApiUrlsManager.kt
@@ -3,6 +3,6 @@ package com.example.mymovies.data.remoteapi
 class ApiUrlsManager {
 	companion object {
 		const val theMovieDbBaseUrl = "https://api.themoviedb.org/3"
-		val imageBaseUrl = "https://image.tmdb.org/t/p"
+		const val imageBaseUrl = "https://image.tmdb.org/t/p"
 	}
 }
