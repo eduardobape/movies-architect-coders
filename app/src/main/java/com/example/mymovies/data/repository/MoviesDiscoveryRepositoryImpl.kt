1f59ce15a773ec03e4da075cda2acc3bfdbc6edc
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-12 13:43:48 +0200

    Create the class implementing the movies discovery repository interface

---
+++ app/src/main/java/com/example/mymovies/data/repository/MoviesDiscoveryRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.example.mymovies.data.repository
+
+import com.example.mymovies.data.remoteapi.models.asDomainModel
+import com.example.mymovies.data.remoteapi.services.MoviesDiscoveryApiService
+import com.example.mymovies.domain.models.MovieMainDetails
+import com.example.mymovies.domain.repository.MoviesDiscoveryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class MoviesDiscoveryRepositoryImpl(private val moviesDiscoveryApiService: MoviesDiscoveryApiService) :
+	MoviesDiscoveryRepository {
+
+	override suspend fun discoverMoviesByYear(
+		releaseYear: Int,
+		page: Int,
+		sortBy: String,
+		region: String,
+		language: String
+	): List<MovieMainDetails> = withContext(Dispatchers.IO) {
+		moviesDiscoveryApiService.discoverMoviesByYear(releaseYear, page, sortBy, region, language).asDomainModel()
+	}
+}
