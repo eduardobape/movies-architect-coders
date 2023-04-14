6cf7339c2c3161745f0173ff6067b381d9ceb13b
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-14 11:55:59 +0200

    Add / at the end of the TheMovieDB API base URL

--- app/src/main/java/com/example/mymovies/data/remoteapi/ApiUrlsManager.kt
+++ app/src/main/java/com/example/mymovies/data/remoteapi/ApiUrlsManager.kt
@@ -2,7 +2,7 @@ package com.example.mymovies.data.remoteapi
 
 class ApiUrlsManager {
 	companion object {
-		const val theMovieDbBaseUrl = "https://api.themoviedb.org/3"
+		const val theMovieDbBaseUrl = "https://api.themoviedb.org/3/"
 		const val imageBaseUrl = "https://image.tmdb.org/t/p"
 	}
 
