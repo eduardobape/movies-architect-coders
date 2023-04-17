deffa53cc41bc4822ba0366fa0ad878c5844e2e4
edudev <3110718+eduardobape@users.noreply.github.com>
2023-04-17 02:20:22 +0200

    Set up the adapter for the recyclerview used to display a list of movies in the MainActivity

--- app/src/main/java/com/example/mymovies/ui/views/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/ui/views/MainActivity.kt
@@ -2,15 +2,26 @@ package com.example.mymovies.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityMainBinding
+	private val moviesAdapter = MoviesAdapter()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
+		setUpMoviesAdapter()
+	}
+
+	private fun setUpMoviesAdapter() {
+		val recyclerViewMovies = binding.rvMoviesList
+		recyclerViewMovies.adapter = moviesAdapter
+		recyclerViewMovies.layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL, false)
+		recyclerViewMovies.addItemDecoration(SpacesItemDecoration(2, 50, true))
 	}
 }
