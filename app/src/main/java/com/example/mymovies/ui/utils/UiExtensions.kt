d0378bfc3dcd7dcfe4ce316621ac73155787334b
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-05-18 13:27:42 +0200

    Create an extension function to load an image into an ImageView using Glide and the URL image

--- app/src/main/java/com/example/mymovies/ui/utils/UiExtensions.kt
+++ app/src/main/java/com/example/mymovies/ui/utils/UiExtensions.kt
@@ -2,7 +2,9 @@ package com.example.mymovies.ui.utils
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 inline fun <reified T> AppCompatActivity.startActivity(intentExtras: Bundle? = null) {
     Intent(this, T::class.java).apply {
@@ -10,4 +12,8 @@ inline fun <reified T> AppCompatActivity.startActivity(intentExtras: Bundle? = n
             putExtras(it)
         }
     }.also(::startActivity)
+}
+
+fun ImageView.loadImageFromUrl(imageUrl: String) {
+    Glide.with(this).load(imageUrl).into(this)
 }
