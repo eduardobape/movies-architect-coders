6b3407734e255d1be9af4150b93ddaf1a13a93b9
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-05-26 13:16:06 +0200

    Move the responsibility of mapping the MoviesDiscoveryResult type from the data layer to the domain layer.
    The MoviesDiscoveryResult type was mapped in the data layer, specifically in the repository, to an appropriated type to be used in the domain layer.
    This responsibility is now moved to this layer, specifically to the DiscoverMoviesUseCase class.

--- app/src/main/java/com/example/mymovies/data/repository/MoviesDiscoveryRepository.kt
+++ app/src/main/java/com/example/mymovies/data/repository/MoviesDiscoveryRepository.kt
@@ -1,20 +1,19 @@
 package com.example.mymovies.data.repository
 
+import com.example.mymovies.data.remoteapi.models.MoviesDiscoveryResult
 import com.example.mymovies.data.remoteapi.services.MoviesDiscoveryApiService
-import com.example.mymovies.domain.models.MoviesDiscoveryDetails
-import com.example.mymovies.domain.models.toDomainModel
 import com.example.mymovies.ui.models.MoviesDiscoveryFilters
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 interface MoviesDiscoveryRepository {
 
-	suspend fun discoverMoviesByYear(moviesDiscoveryFilters: MoviesDiscoveryFilters): MoviesDiscoveryDetails
+	suspend fun discoverMoviesByYear(moviesDiscoveryFilters: MoviesDiscoveryFilters): MoviesDiscoveryResult
 }
 
 class MoviesDiscoveryRepositoryImpl(private val moviesDiscoveryApiService: MoviesDiscoveryApiService) :
 	MoviesDiscoveryRepository {
-	override suspend fun discoverMoviesByYear(moviesDiscoveryFilters: MoviesDiscoveryFilters): MoviesDiscoveryDetails =
+	override suspend fun discoverMoviesByYear(moviesDiscoveryFilters: MoviesDiscoveryFilters): MoviesDiscoveryResult =
 		withContext(Dispatchers.IO) {
 			moviesDiscoveryApiService.discoverMoviesByYear(
 				moviesDiscoveryFilters.releaseYear,
@@ -22,6 +21,6 @@ class MoviesDiscoveryRepositoryImpl(private val moviesDiscoveryApiService: Movie
 				moviesDiscoveryFilters.language,
 				moviesDiscoveryFilters.sortBy,
 				moviesDiscoveryFilters.nextMoviesPageToFetch
-			).toDomainModel()
+			)
 		}
 }
--- app/src/main/java/com/example/mymovies/domain/usecases/DiscoverMoviesUseCase.kt
+++ app/src/main/java/com/example/mymovies/domain/usecases/DiscoverMoviesUseCase.kt
@@ -2,10 +2,11 @@ package com.example.mymovies.domain.usecases
 
 import com.example.mymovies.domain.models.MoviesDiscoveryDetails
 import com.example.mymovies.data.repository.MoviesDiscoveryRepository
+import com.example.mymovies.domain.models.toDomainModel
 import com.example.mymovies.ui.models.MoviesDiscoveryFilters
 
 class DiscoverMoviesUseCase(private val repository: MoviesDiscoveryRepository) {
 
 	suspend operator fun invoke(moviesDiscoveryFilters: MoviesDiscoveryFilters): MoviesDiscoveryDetails =
-		repository.discoverMoviesByYear(moviesDiscoveryFilters)
+		repository.discoverMoviesByYear(moviesDiscoveryFilters).toDomainModel()
 }
