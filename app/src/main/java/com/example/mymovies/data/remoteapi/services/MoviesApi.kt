f6d42b075bb8695b5fe51cdaaf999c377967ce57
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-06-01 00:10:52 +0200

    Create retrofit services using a kotlin extension function of the retrofit library

--- app/src/main/java/com/example/mymovies/data/remoteapi/services/MoviesApi.kt
+++ app/src/main/java/com/example/mymovies/data/remoteapi/services/MoviesApi.kt
@@ -1,14 +1,15 @@
 package com.example.mymovies.data.remoteapi.services
 
 import com.example.mymovies.data.remoteapi.apiclient.RetrofitBuilder
+import retrofit2.create
 
 object MoviesApi {
 
 	val moviesDiscoveryApiService: MoviesDiscoveryApiService by lazy {
-		RetrofitBuilder.retrofit.create(MoviesDiscoveryApiService::class.java)
+		RetrofitBuilder.retrofit.create()
 	}
 
 	val movieDetailsApiService: MovieDetailsApiService by lazy {
-		RetrofitBuilder.retrofit.create(MovieDetailsApiService::class.java)
+		RetrofitBuilder.retrofit.create()
 	}
 }
