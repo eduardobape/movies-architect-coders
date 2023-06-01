f14af855cb87c15880e9ffbee3b8519a795b24cf
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-06-01 16:49:12 +0200

    Sort alphabetically the properties of the MovieDetailsResult class

--- app/src/main/java/com/example/mymovies/data/remoteapi/models/MovieDetailsResult.kt
+++ app/src/main/java/com/example/mymovies/data/remoteapi/models/MovieDetailsResult.kt
@@ -5,14 +5,14 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class MovieDetailsResult(
-    @Json(name = "id") val id: Int,
-    @Json(name = "title") val translatedTitle: String,
-    @Json(name = "original_title") val originalTitle: String,
-    @Json(name = "poster_path") val posterPath: String?,
     @Json(name = "backdrop_path") val backdropImagePath: String?,
-    @Json(name = "overview") val overview: String?,
-    @Json(name = "release_date") val releaseDate: String,
     @Json(name = "genres") val genres: List<MovieGenre>,
+    @Json(name = "id") val id: Int,
+    @Json(name = "original_title") val originalTitle: String,
+    @Json(name = "overview") val overview: String?,
+    @Json(name = "poster_path") val posterPath: String?,
+    @Json(name = "release_date") val releaseDate: String,
+    @Json(name = "title") val translatedTitle: String,
     @Json(name = "vote_average") val voteAverage: Float
 )
 
