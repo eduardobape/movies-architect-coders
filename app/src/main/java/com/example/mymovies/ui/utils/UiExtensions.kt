28309b30688f560b8808ddbe46f52cda5d7ae433
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-06-17 16:39:00 +0200

    Move the extension function 'Fragment.viewLifecycleBinding' to other file.
    This function is moved from the ViewBindingFragmentDelegate.kt file to the UiExtensions.kt file.

--- app/src/main/java/com/example/mymovies/ui/utils/UiExtensions.kt
+++ app/src/main/java/com/example/mymovies/ui/utils/UiExtensions.kt
@@ -6,7 +6,12 @@ import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.bumptech.glide.Glide
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 inline fun <reified T> AppCompatActivity.startActivity(intentExtras: Bundle? = null) {
     Intent(this, T::class.java).apply {
@@ -24,4 +29,29 @@ var View.visible: Boolean
     get() = isVisible
     set(value) {
         visibility = if (value) View.VISIBLE else View.GONE
+    }
+
+fun <T> Fragment.viewLifecycleBinding(initialise: () -> T): ReadOnlyProperty<Fragment, T> =
+    object : ReadOnlyProperty<Fragment, T>, DefaultLifecycleObserver {
+
+        private var binding: T? = null
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            binding = null
+            this@viewLifecycleBinding
+                .viewLifecycleOwner
+                .lifecycle
+                .removeObserver(this)
+            super.onDestroy(owner)
+        }
+
+        override fun getValue(
+            thisRef: Fragment,
+            property: KProperty<*>
+        ): T {
+            return binding ?: initialise().also {
+                binding = it
+                this@viewLifecycleBinding.viewLifecycleOwner.lifecycle.addObserver(this)
+            }
+        }
     }
