021142d1accaca193e9b79b99e26e23099379db6
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-07-03 21:15:18 +0200

    Fix bug that hid the progress bar when loading popular movies

--- app/src/main/java/com/example/mymovies/ui/views/MainFragment.kt
+++ app/src/main/java/com/example/mymovies/ui/views/MainFragment.kt
@@ -2,7 +2,6 @@ package com.example.mymovies.ui.views
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
@@ -72,7 +71,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private fun updateViewsFromUiState(uiState: PaginatedMoviesMainUiState) {
         moviesAdapter.submitList(uiState.movies)
         binding.pbMoviesList.visible = uiState.isLoading
-        binding.pbMoviesList.isVisible = false
     }
 
     private fun onScrollMovies() {
