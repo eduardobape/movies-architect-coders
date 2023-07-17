8827b6cc6a0eba5976ccdfedef0e870912542dcf
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-07-17 20:47:31 +0200

    Remove withContext(Dispatchers.IO) from the getMovieDetails method.
    There is no need to use Dispatchers.IO because Retrofit executes http requests on background threads.

--- app/src/main/java/com/example/mymovies/data/repository/MovieDetailsRemoteRepository.kt
+++ app/src/main/java/com/example/mymovies/data/repository/MovieDetailsRemoteRepository.kt
@@ -3,15 +3,10 @@ package com.example.mymovies.data.repository
 import com.example.mymovies.data.remote.models.MovieDetailsResult
 import com.example.mymovies.data.remote.services.MovieDetailsApiService
 import com.example.mymovies.domain.repositories.MovieDetailsRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class MovieDetailsRemoteRepository(private val movieDetailsApiService: MovieDetailsApiService) :
     MovieDetailsRepository {
 
-    override suspend fun getMovieDetails(movieId: Int): MovieDetailsResult {
-        return withContext(Dispatchers.IO) {
-            movieDetailsApiService.getMovieDetails(movieId)
-        }
-    }
+    override suspend fun getMovieDetails(movieId: Int): MovieDetailsResult =
+        movieDetailsApiService.getMovieDetails(movieId)
 }
