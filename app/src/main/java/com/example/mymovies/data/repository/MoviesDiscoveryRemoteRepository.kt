a811b19364b68cced58204f1c57ece0d6446737b
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-07-17 20:42:05 +0200

    Remove withContext(Dispatchers.IO) from the getPopularMovies method.
    There is no need to use Dispatchers.IO because Retrofit executes http requests on background threads.

--- app/src/main/java/com/example/mymovies/data/repository/MoviesDiscoveryRemoteRepository.kt
+++ app/src/main/java/com/example/mymovies/data/repository/MoviesDiscoveryRemoteRepository.kt
@@ -4,8 +4,6 @@ import com.example.mymovies.data.remote.models.MoviesDiscoveryResult
 import com.example.mymovies.data.remote.services.MoviesDiscoveryApiService
 import com.example.mymovies.domain.repositories.MoviesDiscoveryRepository
 import com.example.mymovies.ui.models.MoviesDiscoveryFilters
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class MoviesDiscoveryRemoteRepository(private val moviesDiscoveryApiService: MoviesDiscoveryApiService) :
     MoviesDiscoveryRepository {
@@ -14,11 +12,9 @@ class MoviesDiscoveryRemoteRepository(private val moviesDiscoveryApiService: Mov
         moviesDiscoveryFilters: MoviesDiscoveryFilters,
         pageToFetch: Int
     ): MoviesDiscoveryResult =
-        withContext(Dispatchers.IO) {
-            moviesDiscoveryApiService.getPopularMovies(
-                moviesDiscoveryFilters.region,
-                moviesDiscoveryFilters.language,
-                pageToFetch
-            )
-        }
+        moviesDiscoveryApiService.getPopularMovies(
+            moviesDiscoveryFilters.region,
+            moviesDiscoveryFilters.language,
+            pageToFetch
+        )
 }
