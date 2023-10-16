36a1175da003700f1576d88748c241fff66195bb
EduDev <3110718+eduardobape@users.noreply.github.com>
2023-10-17 00:22:40 +0200

    Move NetworkChecker.kt file to other package

--- app/src/main/java/com/example/mymovies/framework/webservice/NetworkChecker.kt
+++ app/src/main/java/com/example/mymovies/framework/webservice/NetworkChecker.kt
@@ -1,4 +1,4 @@
-package com.example.mymovies.framework.shared
+package com.example.mymovies.framework.webservice
 
 import android.content.Context
 import android.net.ConnectivityManager
--- app/src/main/java/com/example/mymovies/framework/webservice/RemoteConnection.kt
+++ app/src/main/java/com/example/mymovies/framework/webservice/RemoteConnection.kt
@@ -3,7 +3,6 @@ package com.example.mymovies.framework.webservice
 import com.example.mymovies.App
 import com.example.mymovies.BuildConfig
 import com.example.mymovies.appContext
-import com.example.mymovies.framework.shared.NetworkChecker
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
